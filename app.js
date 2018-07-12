77716d28df9fe1cff35943ce8008f9e3a3ef7e2e
philippe <philippe@SIMOCCAUCH26.lan>
2018-07-12 15:38:17 +0200

    addimg

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-
-
 console.log("hello");
-//fonction qui ajoute les photos dans le localstorage et qui les affiche pr l'user.
-function ajouterPhoto(){
-	var photo = JSON.parse(window.localStorage.getItem("galerie"));
-	if (photo == null) 
-	{
-		var photo = [];
-	}
-	photo.push($("#inPhoto").val());
-	for (var i = 0; i < photo.length; i++)
-		window.localStorage.setItem("galerie", JSON.stringify(photo));
-	{
-		$("#divPhoto").append(photo);
-		console.log(photo);  
-	}
-};
+
+
 
 $("#buttonAjout").click(function(){
 
 	ajouterPhoto();
 	$("#divPhoto").show();
 });
+//fonction qui ajoute les photos dans le localstorage et qui les affiche pr l'user.
+function ajouterPhoto(){
+	$("#divPhoto").empty();
+	var photo = JSON.parse(window.localStorage.getItem("galerie"));
+	if (photo == null) 
+	{
+		var photo = [];
+	}
 
+	var nouvelle_image = document.getElementById("inPhoto").files[0].name;
+ 
+
+	photo.push( nouvelle_image );
+
+	window.localStorage.setItem("galerie", JSON.stringify(photo));
+
+	for (var i = 0; i < photo.length; i++)
+	{
+		$("#divPhoto").append('<img src="images/' + photo[i] + '"/>');
+	}
+};
